f17a83d38890c412bea7a16933b07f4f94d78c65
Anthony Macias <Anthony40macias@gmail.com>
2024-02-04 09:42:45 -0800

    defined user-controllers function.

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,2 +1,17 @@
-export const getAllUsers = async () => {
-}
+import  User from "../models/User";
+import { Request, Response, NextFunction } from "express";
+
+export const getAllUsers = async (
+    req:Request, 
+    res:Response, 
+    next:NextFunction
+    ) => {
+    try {
+        // get all users
+        const users = await User.find({});
+        return res.status(200).json({message:"ok",users});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message:"ERROR", cause: "An error occurred" });
+    }
+};
