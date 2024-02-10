1ff03233b4f265b7549ca942d0472568a84347b5
Anthony Macias <Anthony40macias@gmail.com>
2024-02-09 20:21:29 -0800

    frontend has newer routes and components. Basic setup before coding out the frontend

--- frontend/webwhisperingchat/src/App.tsx
+++ frontend/webwhisperingchat/src/App.tsx
@@ -1,8 +1,24 @@
+import Header from './component/Header'
 import './App.css'
+import { Routes } from'react-router-dom';
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Chat from './pages/Chat'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <p>Hello world! </p>
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/chat" element={<Chat/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </main>
   )
 }
 
